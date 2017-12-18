b4b7189bb94763f18598b4be598fa3a3387af5e3
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-12-17 22:05:24 -0800

    add model and controller for park

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,38 @@
+class ParksController < ApplicationController
+  before_action :find_park, only: [:show, :update, :destroy]
+
+   def index
+    parks = Park.all
+    json_response(parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @park = Park.create(Park_params)
+    json_response(@park)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    @park.update(Park_params)
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
+  end
+
+  private
+
+  def park_params
+    params.permit(:name, :state)
+  end
+
+  def find_park
+    @park = Park.find(params[:id])
+  end
+end
---
+++ app/models/park.rb
@@ -0,0 +1,6 @@
+class Park < ApplicationRecord
+
+
+  validates_presence_of :name, :state
+
+end
